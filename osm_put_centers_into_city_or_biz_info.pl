177eb3eaaa7b26349d9391bce2894f809e4b1246
Richard Fobes <prog@SolutionsCreative.com>
2023-04-18 13:32:09 -0700

    Refine copyright notice

--- osm_put_centers_into_city_or_biz_info.pl
+++ osm_put_centers_into_city_or_biz_info.pl
@@ -2,7 +2,18 @@
 #       osm_put_centers_into_city_or_biz_info.pl
 #--------------------------------------------------
 
-#  (c) Copyright 2022 by Richard Fobes at SolutionsCreative.com
+#  (c) Copyright 2022-2023 by Richard Fobes at SolutionsCreative.com
+#  Permission to copy and use and modify this
+#  software is hereby given to individuals and to
+#  businesses with ten or fewer employees if this
+#  copyright notice is included in all copies
+#  and modified copies.
+#  All other rights are reserved.
+#  Businesses with more than ten employees are
+#  encouraged to contract with small businesses
+#  to supply the service of running this software
+#  if they also arrange to make donations to
+#  support the Open Street Map project.
 
 #  Usage:
 #
